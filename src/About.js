8a43d6a7df029df14215f69c3eeaaeb029ac75bc
AJ Dorfman <ajdorfman91@gmail.com>
2023-12-11 16:20:36 -0800

    proofread and clean up

--- src/About.js
+++ src/About.js
@@ -3,11 +3,11 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about">
-      <img src="/sunflower1.jpg" alt="Sunflower" className="center-image" />
+      <img src="/sunflower1.jpg" alt="Sunflower" className="center-image" style={{ width: '100%' }} />
       <h2 className="center-text">Tarin McCarthy</h2>
       <p>
         I am a Licensed Clinical Social Worker (LCSW) dedicated to empowering individuals on their journey to emotional well-being. With a compassionate and empathetic approach, I create a safe and non-judgmental space for clients to explore their thoughts and feelings.  
-        I specialize in helping clients navigate issues related to anxiety, depression, stress management, and relationship difficulties. I believe in solution-focused therapy with a client-centered approach to help you achieve your goals and move forward with living your best life.  
+        I specialize in helping clients navigate issues related to anxiety, depression, stress management, and relationship difficulties. I believe in Solution-Focused therapy with a Client-Centered approach to help you achieve your goals and move forward with living your best life.  
         By integrating evidence-based practices, mindfulness techniques, and a strengths-based perspective, I can assist in building resilience, enhancing self-awareness, and fostering meaningful connections in your life.
       </p>
     </div>
